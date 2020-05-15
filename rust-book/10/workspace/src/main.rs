38956907c7d79b1a435d60a3c8b776c1207292e1
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2020-05-15 20:43:43 +0900

    Update

---
+++ rust-book/10/workspace/src/main.rs
@@ -0,0 +1,85 @@
+use std::cmp::PartialOrd;
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![43, 123, 423, 34, 41, 341];
+    let largest = largest(&number_list);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    println!("Hello, world! {}?", largest);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        // もちろん、ご存知かもしれないようにね、みなさん
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    {
+        let r;
+
+        let x = 5;
+        r = &x;
+
+        println!("{}", r);
+    }
+
+    let c;
+    let a = String::from("AAA");
+    {
+        let b = "BBB";
+        c = longest(a.as_str(), b);
+    }
+    println!("{}", c);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
