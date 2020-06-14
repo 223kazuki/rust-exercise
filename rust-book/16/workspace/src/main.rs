5126ca270e223d9aaa743307d18a3ddf63ba1688
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2020-06-14 21:38:59 +0900

    Today

--- rust-book/16/workspace/src/main.rs
+++ rust-book/16/workspace/src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -19,4 +20,37 @@ fn main() {
         thread::sleep(Duration::from_millis(1));
     }
     handler.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("message"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
